22f6deb377e6bd309d6c6f7b926f3ea039d66ac3
Nile <66771424+Hashdive@users.noreply.github.com>
2021-04-16 16:55:33 -0400

    Intro Scene Add
    Added Intro Scene before toying around with Nethereum

---
+++ Delivery_Unity/Assets/Quiet Hill/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    // Birdman spawn & location variables
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+
+    // spawn rate and limit variables
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    public int spawnLimit;
+    public int enemies;
+    public int deadBirds = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check number of Birdmen
+        enemies = GameObject.FindGameObjectsWithTag("Birdman").Length;
+        
+        // wait for intro to end
+        if (Time.timeSinceLevelLoad >= 20)
+        {
+            //stagger spawns & limit total number & check that spawn time is correct
+            if (Time.time > nextSpawn && enemies < spawnLimit && Mathf.Floor(Time.time) % 4 == 0)
+            {
+                // reset next spawn
+                nextSpawn = Time.time + spawnRate;
+
+                // randomize spawn location within horizontal range
+                randX = Random.Range (-8f, 4f);
+
+                // set spawn location
+                whereToSpawn = new Vector2 (randX, transform.position.y);
+
+                // spawn
+                GameObject clone = Instantiate (enemy, whereToSpawn, Quaternion.identity);
+
+                // assign layer
+                clone.layer = LayerMask.NameToLayer("Birdman");
+            }
+
+            // when Birdman dies, alternate respawn
+            if (Time.time > nextSpawn - 2 && deadBirds >= 3 && Mathf.Floor(Time.time) % 2 == 0)
+            {
+                // randomize spawn location within horizontal range
+                randX = Random.Range (-8f, 4f);
+
+                // set spawn location
+                whereToSpawn = new Vector2 (randX, transform.position.y);
+
+                // spawn
+                GameObject clone = Instantiate (enemy, whereToSpawn, Quaternion.identity);
+
+                // assign layer
+                clone.layer = LayerMask.NameToLayer("Birdman");
+
+                // reduce respawn limit
+                deadBirds--;
+            }
+
+            // increase spawn limit
+            if (Time.time >= 140){
+                spawnLimit = 12;
+            }
+
+            // increase spawn limit again
+            if (Time.time >= 200){
+                spawnLimit = 12;
+            }
+
+            // destroy enemies when player wins
+            if (Time.time >= 260){
+                GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Birdman");
+                for (int i=0; i<enemies; i++){
+                    Destroy(allEnemies[i]);
+                }
+            }
+        }
+
+        // return during intro
+        else
+        {
+            return;
+        }
+    }
+
+    // increase respawn limit
+    public void deadBird(){
+        deadBirds++;
+	}
+
+}
---
+++ Delivery_Unity/Assets/Quiet Hill/Scripts/TitleScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+	public float count;
+	private float currentPos;
+
+	private int titleState;
+
+	public GameObject logo;
+	public GameObject canvas;
+	public GameObject enterText;
+	public GameObject about;
+
+	// Use this for initialization
+	void Start () {
+		titleState = 1;
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return) ){
+			if (titleState == 1) {
+				titleState = 2;
+				about.SetActive (true);
+				logo.SetActive (false);
+				enterText.SetActive (false);
+				canvas.SetActive (false);
+			} else {
+				SceneManager.LoadScene("scene-1");
+			}
+		}
+
+	}
+}
